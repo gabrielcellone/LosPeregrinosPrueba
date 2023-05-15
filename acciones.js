840aabe565a89fdcc604d2905087e7ca8704d2e7
Gabriel Cellone <leviatan@tutanota.com>
2023-05-14 22:53:00 -0300

    Mayo14

--- acciones.js
+++ acciones.js
@@ -1,7 +1,12 @@
-function mostrarTexto() {
-    document.getElementById('texto_Eol').style.display = 'block';
-   
-  }
-    function ocultarTexto() {
-       document.getElementById('texto_Eol').style.display = 'none';
+
+        function mostrarTexto() {
+          document.getElementById("texto_Eol").style.display = "block";
+        }
+        
+        function ocultarTexto() {
+          document.getElementById("texto_Eol").style.display = "none";
+        }
+        
+        function desplegarCancion(cancion) {
+          alert("Desplegando información de la canción: " + cancion);
         }
