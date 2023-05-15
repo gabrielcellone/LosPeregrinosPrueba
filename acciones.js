117207afb1e5577a8d20a50499bb215c9bfb350e
gabrielcellone <124008317+gabrielcellone@users.noreply.github.com>
2023-05-15 12:59:56 -0300

    Add files via upload

---
+++ acciones.js
@@ -0,0 +1,12 @@
+
+        function mostrarTexto() {
+          document.getElementById("texto_Eol").style.display = "block";
+        }
+        
+        function ocultarTexto() {
+          document.getElementById("texto_Eol").style.display = "none";
+        }
+        
+        function desplegarCancion(cancion) {
+          alert("Desplegando información de la canción: " + cancion);
+        }
